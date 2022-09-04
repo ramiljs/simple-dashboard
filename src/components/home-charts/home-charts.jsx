fa37882dd5307c1fbb22384c8d687d3534ec442c
Ramil <89657343+kama1ov@users.noreply.github.com>
2022-09-04 09:53:06 +0500

    Update home-charts.jsx

--- src/components/home-charts/home-charts.jsx
+++ src/components/home-charts/home-charts.jsx
@@ -1,20 +1,6 @@
 import React from "react";
-import { 
-  Card, 
-  Stack, 
-  CardActionArea 
-} from "@mui/material";
-import { 
-  Bar, 
-  Line, 
-  XAxis, 
-  YAxis, 
-  Legend, 
-  Tooltip, 
-  BarChart, 
-  LineChart, 
-  CartesianGrid 
-} from 'recharts';
+import { Card, Stack, CardActionArea } from "@mui/material";
+import { Bar, Line, XAxis, YAxis, Legend, Tooltip, BarChart, LineChart, CartesianGrid } from 'recharts';
 
 function HomeCharts() {
   const data = [
@@ -25,10 +11,19 @@ function HomeCharts() {
 
   return (
     <div className="HomeCharts">
-      <Stack direction="row" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '10px 0 25px 240px' }} spacing={5}>
+      <Stack 
+        direction="row" 
+        sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '10px 0 25px 240px' }} 
+        spacing={5}
+      >
         <Card sx={{ width: '500px' }}>
           <CardActionArea>
-            <LineChart width={500} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+            <LineChart 
+              width={500} 
+              height={300} 
+              data={data} 
+              margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+            >
               <Line type="monotone" dataKey="uv" stroke="#8884d8" />
               <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
               <XAxis dataKey="name" />
@@ -43,7 +38,10 @@ function HomeCharts() {
               <XAxis dataKey="name" stroke="#8884d8" />
               <YAxis />
               <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
-              <Legend width={70} wrapperStyle={{ top: 40, right: -30, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
+              <Legend 
+                width={70} 
+                wrapperStyle={{ top: 40, right: -30, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} 
+              />
               <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
               <Bar dataKey="uv" fill="#8884d8" barSize={30} />
             </BarChart>
