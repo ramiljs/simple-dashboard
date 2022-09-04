e7e78eb73bf2f62728a8faca9b7caa5497ac48fe
Ramil <89657343+kama1ov@users.noreply.github.com>
2022-09-04 09:45:52 +0500

    Update home-header.jsx

--- src/components/home-header/home-header.jsx
+++ src/components/home-header/home-header.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import { 
-  Box, 
-  AppBar, 
-  Toolbar, 
-  TextField, 
-  IconButton 
-} from '@mui/material';
+import { Box, AppBar, Toolbar, TextField, IconButton } from '@mui/material';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
@@ -13,7 +7,10 @@ import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 function HomeHeader() {
   return(
     <Box sx={{ flexGrow: 1, margin: '0 0 10px 240px' }}>
-      <AppBar  sx={{ display: 'flex', justifyContent: 'center', width: '100%', height: '70px', backgroundColor: '#fff' }} position="static">
+      <AppBar 
+        sx={{ display: 'flex', justifyContent: 'center', width: '100%', height: '70px', backgroundColor: '#fff' }} 
+        position="static"
+      >
         <Toolbar variant="dense">
           <IconButton edge="start" aria-label="menu" sx={{ mr: 2 }}>
             <KeyboardBackspaceIcon sx={{ fontSize: '30px' }} />
