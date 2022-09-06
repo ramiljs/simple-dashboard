0b180bb1528624e18c4c04e195d9eb3b547d601b
Ramil <89657343+kamalovvvv@users.noreply.github.com>
2022-09-06 06:08:54 +0500

    Update home-drawer.jsx

--- src/components/home-drawer/home-drawer.jsx
+++ src/components/home-drawer/home-drawer.jsx
@@ -1,40 +1,52 @@
-import React from "react";
-import "./style.css";
-import admin from "../../assets/images/admin.jpg";
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-import CurrencyExchangeOutlinedIcon from '@mui/icons-material/CurrencyExchangeOutlined';
-import { sidebarMainLinks } from "../../constants/sidebar-links";
-import { Box, List, Stack, Avatar, Drawer, ListItem, Typography, IconButton, ListItemIcon, ListItemText } from '@mui/material';
+import React from 'react'
+import './style.css'
+import admin from '../../assets/images/admin.jpg'
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined'
+import CurrencyExchangeOutlinedIcon from '@mui/icons-material/CurrencyExchangeOutlined'
+import { sidebarMainLinks } from "../../constants/sidebar-links"
+import { Box, List, Stack, Avatar, Drawer, ListItem, Typography, IconButton, ListItemIcon, ListItemText } from '@mui/material'
 
 function HomeDrawer() {
   return (
     <Box>
       <Drawer
         sx={{ width: 240, flexShrink: 0, '& .MuiDrawer-paper': { width: 240, boxSizing: 'border-box' } }}
-        variant="permanent"
-        anchor="left"
+        variant='permanent'
+        anchor='left'
       >
         <Stack
-          sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '10px 0' }} 
-          direction="column" 
+          sx={{ 
+            display: 'flex', 
+            alignItems: 'center', 
+            justifyContent: 'center', 
+            margin: '10px 0' 
+          }} 
+          direction='column' 
           spacing={0}
         >
           <Avatar 
             sx={{ width: '55px', height: '55px', borderRadius: '10px' }} 
             src={admin} 
-            alt="Admin Sale" 
+            alt='Admin Sale' 
           />
           <ListItemIcon sx={{ marginTop: '-45px', columnGap: '70px' }}>
             <IconButton>
-              <SettingsOutlinedIcon fontSize="small" />
+              <SettingsOutlinedIcon fontSize='small' />
             </IconButton>
             <IconButton>
-              <NotificationsNoneOutlinedIcon fontSize="small" />
+              <NotificationsNoneOutlinedIcon fontSize='small' />
             </IconButton>
           </ListItemIcon>
-          <Typography noWrap component="div" variant="h6" sx={{ marginTop: '10px' }}> John Doe </Typography>
-          <Typography noWrap component="div" sx={{ fontSize: '12px' }}> +998 (99) 123 10 11 </Typography>
+          <Typography 
+            noWrap 
+            component='div' 
+            variant='h6' 
+            sx={{ marginTop: '10px' }}
+          >
+            John Doe
+          </Typography>
+          <Typography noWrap component='div' sx={{ fontSize: '12px' }}> +998 (99) 123 10 11 </Typography>
           <ListItem button style={{ display: 'inline', backgroundColor: '#dcedc8' }}>
             <Typography sx={{ fontSize: '12px', fontWeight: 'bold' }}> Wallet balance </Typography>
             <Typography sx={{ fontSize: '16px', fontWeight: 'bold' }}> $4238 </Typography>
@@ -55,7 +67,7 @@ function HomeDrawer() {
         </List>
       </Drawer>
     </Box>
-  );
+  )
 }
 
-export default HomeDrawer;
+export default HomeDrawer
